85301461d0737a75968e44a70f6994b62cefbecf
successful_fella <preetjuneja@zingalbox.com>
2020-08-03 23:04:50 +0530

    I like this color better

--- 04 Import NPM Module Chalk/app.js
+++ 04 Import NPM Module Chalk/app.js
@@ -2,6 +2,6 @@ const chalk = require("chalk")
 
 const error = chalk.redBright.bold
 
-console.log(chalk.blue("Kawalpreet", " ", chalk.underline.inverse.bgWhite("Juneja")))
+console.log(chalk.cyanBright("Kawalpreet", chalk.underline.inverse("Juneja")))
 
 console.log(error("Error! Error! Error!"))
